eb8e279c1bcf52ff7e9b62b4ee4492d68e7c72bb
ericsonmoreira <moreira.ericson@gmail.com>
2020-08-14 15:31:58 -0300

    #20 removendo coisas desnecessárias

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,1 @@
-export { default as chartjs } from './chartjs';
-export { default as getInitials } from './getInitials';
-export { default as cpfMask } from './cpfMask';
+export { default as formatDate } from './formatDate';
