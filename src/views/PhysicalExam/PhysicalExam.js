01c693c03f29995762e3f2c7dd6b37650f18bc5d
Felipe Silva <felipe.salaberga@gmail.com>
2020-08-20 17:23:55 -0300

    #10 - Adjusting breadcrumbs

--- src/views/PhysicalExam/PhysicalExam.js
+++ src/views/PhysicalExam/PhysicalExam.js
@@ -77,6 +77,7 @@ const PhysicalExam = () => {
           links={[
             { label: 'Meus pacientes', route: '/meus-pacientes' },
             { label: 'Categorias', route: '/categorias' },
+            { label: 'Lista de evoluções', route: '/categorias/lista-exame-fisico' },
             { label: 'Exame Físico', route: '/categorias/exame-fisico' },
           ]}
         />
