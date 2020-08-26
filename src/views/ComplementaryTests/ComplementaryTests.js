88bdbc880424548028843c236b67ee793265cc3f
ericsonmoreira <moreira.ericson@gmail.com>
2020-08-26 16:16:23 -0300

    #11 add Title Wrapper

--- src/views/ComplementaryTests/ComplementaryTests.js
+++ src/views/ComplementaryTests/ComplementaryTests.js
@@ -1,9 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { usePatient } from 'context/PatientContext';
+import useStyles from './styles';
+import { CustonBreadcrumbs } from 'components';
+import { CircularProgress, FormControl, Typography, Grid, Button } from '@material-ui/core';
+import { Formik, Form } from 'formik';
+import schema from './schema';
+import PatientInfo from 'components/PatientInfo';
+
+// Valores iniciais
+const initialValues = {};
 
 function ComplementaryTests() {
+  const { patient } = usePatient();
+
+  const { id } = patient;
+
+  const classes = useStyles();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleComplementaryTests = useCallback(async id => {
+    try {
+      setLoading(true);
+      console.log(id);
+    } catch (err) {
+      // TODO: tratar os erros do carregamento aqui.
+    } finally {
+      setLoading(false);
+    }
+  });
+
+  // TODO: nada ainda
+  const handleSubmit = values => {
+    console.log('handleSubmit', values);
+  };
+
+  useEffect(() => {
+    handleComplementaryTests(id);
+  }, [handleComplementaryTests, id]);
+
   return (
-    <div>
-      <h1>Ola ComplementaryTests</h1>
+    <div className={classes.root}>
+      <div className={classes.header}>
+        <CustonBreadcrumbs
+          links={[
+            { label: 'Meus pacientes', route: '/meus-pacientes' },
+            { label: 'Categorias', route: '/categorias' },
+            {
+              label: 'Exames laboratoriais complementares',
+              route: '/categorias/exames-complementares/',
+            },
+          ]}
+        />
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <div className={classes.formWrapper}>
+            <Formik
+              enableReinitialize
+              initialValues={initialValues}
+              onSubmit={handleSubmit}
+              validateOnMount
+              validationSchema={schema}
+            >
+              {({isSubmitting}) => (
+                <Form component={FormControl}>
+                  <div className={classes.titleWrapper}>
+                    <Typography variant="h2">
+                      Exames laboratoriais complementares
+                    </Typography>
+                    <Grid
+                      className={classes.actionSection}
+                      item
+                    >
+                      <PatientInfo />
+                      <Button
+                        className={classes.buttonSave}
+                        color="secondary"
+                        disabled={isSubmitting}
+                        type="submit"
+                        variant="contained"
+                      >
+                        Salvar
+                      </Button>
+                    </Grid>
+                  </div>
+
+                </Form>
+              )}
+            </Formik>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
