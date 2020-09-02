c6bda8117766f920c5fb04842d1fb4f6aa00c857
ericsonmoreira <moreira.ericson@gmail.com>
2020-09-02 18:08:40 -0300

    #16 add estrutura inicial do layout da página

--- src/views/ RelatedInfections/RelatedInfections.js
+++ src/views/ RelatedInfections/RelatedInfections.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { usePatient } from 'context/PatientContext';
+import useStyles from './styles';
+import { CustonBreadcrumbs } from 'components';
+import { CircularProgress } from '@material-ui/core';
+import { Formik } from 'formik';
+import schema from './schema';
 
+// TODO: nada por enquanto
+const initialValues = {};
+
+// Component da pgiande Infecções relacionadas à assistência à saúde (IRAS)
 const RelatedInfections = () => {
-  return <div>RelatedInfections</div>;
+  const {
+    patient: { id },
+  } = usePatient();
+
+  const classes = useStyles();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleRelatedInfections = useCallback(async id => {
+    try {
+      setLoading(true);
+      console.log(id);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const handleSubmit = useCallback(async values => {
+    try {
+      // TODO: mudar isso depois
+      console.log(values);
+    } catch (error) {
+      // TODO: melhorar o tratamento do erro aqui
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleRelatedInfections(id);
+  }, [handleRelatedInfections, id]);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.header}>
+        <CustonBreadcrumbs
+          links={[
+            { label: 'Meus pacientes', route: '/meus-pacientes' },
+            { label: 'Categorias', route: '/categorias' },
+            {
+              label: 'Infecções relacionadas à assistência à saúde (IRAS)',
+              route: '/categorias/iras/',
+            },
+          ]}
+        />
+      </div>
+
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <div className={classes.formWrapper}>
+          <Formik
+            enableReinitialize
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validateOnMount
+            validationSchema={schema}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RelatedInfections;
---
+++ src/views/ RelatedInfections/schema.js
@@ -0,0 +1,25 @@
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  // newsTestsRTCPRs: Yup.array().of(
+  //   Yup.object().shape({
+  //     data_coleta: Yup.string().required('Campo obrigatório'),
+  //     sitio_tipo: Yup.number().required(
+  //       'Campo obrigatório. Você deve preenchê-lo para salvar os dados.',
+  //     ),
+  //     data_resultado: Yup.string(),
+  //     rt_pcr_resultado: Yup.number(),
+  //   }),
+  // ),
+  // newsTestsRapidos: Yup.array().of(
+  //   Yup.object().shape({
+  //     data_realizacao: Yup.string().required('Campo obrigatório'),
+  //     resultado: Yup.string().required(
+  //       'Campo obrigatório. Você deve preenchê-lo para salvar os dados.',
+  //     ),
+  //   }),
+  // ),
+  // tipo_new_teste: Yup.string(),
+});
+
+export default schema;
