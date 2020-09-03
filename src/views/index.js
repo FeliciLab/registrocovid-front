566bb842e30163c02f760a4b650ac13e14b4fef3
MatheusVarela19 <matheus.varela@aluno.uece.br>
2020-09-03 17:04:04 -0300

    #16 minor correcion at folder name 'RelatedInfections'

--- src/views/index.js
+++ src/views/index.js
@@ -10,5 +10,5 @@ export { default as PhysicalExam } from './PhysicalExam';
 export { default as InitialSymptoms } from './InitialSymptoms';
 export { default as Comorbidities } from './Comorbidities';
 export { default as ComplementaryTests } from './ComplementaryTests';
-export { default as RelatedInfections } from './ RelatedInfections';
+export { default as RelatedInfections } from './RelatedInfections';
 export { default as PhysicalExamsList } from './PhysicalExamsList';
