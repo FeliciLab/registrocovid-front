12f06ea5d5773b088082099538d06e0947bbedb0
Edely Gomes <edelygomes@gmail.com>
2020-09-09 18:35:59 -0300

    Adiciona formatação condicional com yup

--- src/views/Complications/schema.js
+++ src/views/Complications/schema.js
@@ -1,10 +1,23 @@
-import * as Yup from 'yup';
+import { object, string, number, boolean, array } from 'yup';
 
-const schema = Yup.object().shape({
-  newsComplicacoes: Yup.array().of(
-    Yup.object().shape({
-      tipo_complicacao_id: Yup.string().required('Campo obrigatório'),
-      glasglow_admissao_uti: Yup.string().required('Campo obrigatório'),
+const schema = object().shape({
+  newsComplicacoes: array().of(
+    object().shape({
+      tipo_complicacao_id: string().required('Campo obrigatório'),
+      glasglow_admissao_uti: number().when('tipo_complicacao_id', {
+        is: '1',
+        then: number().required('Campo obrigatório'),
+      }),
+      data: string().required('Campo obrigatório'),
+      data_termino: string().required('Campo obrigatório'),
+      descricao: string().when('tipo_complicacao_id', {
+        is: '13',
+        then: string().required('Campo obrigatório'),
+      }),
+      menos_24h_uti: boolean().when('tipo_complicacao_id', {
+        is: '1',
+        then: boolean().required('Campo obrigatório'),
+      }),
     }),
   ),
 });
