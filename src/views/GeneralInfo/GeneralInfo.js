8d536bcb216ec4e3c8668a25e1c6047026f6e18c
Dayany Espíndola <dcortes@thoughtworks.com>
2020-09-15 19:35:03 -0300

    #72 Exibe informações gerais assim que novo paciente é cadastrado

--- src/views/GeneralInfo/GeneralInfo.js
+++ src/views/GeneralInfo/GeneralInfo.js
@@ -112,12 +112,26 @@ const GeneralInfo = () => {
         created_at: formatDate(response.data.paciente.created_at)
       };
 
+      const complementaryResponsePatient = {
+        ...responsePatient,
+
+        data_internacao: values.data_internacao,
+        suporte_respiratorio: values.suporte_respiratorio,
+        reinternacao: values.reinternacao,
+
+        instituicao_primeiro_atendimento: { id: values.unidade_primeiro_atendimento },
+        instituicao_referencia: { id: values.unidade_de_saude },
+        data_atendimento_referencia: values.data_atendimento,
+        tipo_suporte_respiratorios: [{ id: values.tipo_suport_respiratorio }],
+      }
+
       addToast({
         type: 'success',
         message: 'Dados salvos com sucesso',
       });
 
-      addPatient(responsePatient);
+      addPatient(complementaryResponsePatient);
+
       history.push('/categorias');
     } catch (err) {
       if (err.response.data.message === 'The given data was invalid.') {
@@ -135,16 +149,32 @@ const GeneralInfo = () => {
   };
 
   const loadInitialValues = () => {
-    return {
-      prontuario: patient && patient.prontuario ? patient.prontuario : '',
-      data_internacao: patient && patient.data_internacao ? patient.data_internacao : '',
-      unidade_primeiro_atendimento: patient && patient.instituicao_primeiro_atendimento ? patient.instituicao_primeiro_atendimento.id : '',
-      unidade_de_saude: patient && patient.instituicao_referencia ? patient.instituicao_referencia.id : '',
-      data_atendimento: patient && patient.data_atendimento_referencia ? patient.data_atendimento_referencia : '',
-      suporte_respiratorio: patient && patient.suporte_respiratorio ? patient.suporte_respiratorio : false,
-      tipo_suport_respiratorio: patient && patient.tipo_suporte_respiratorios ? patient.tipo_suporte_respiratorios[0].id : '',
-      reinternacao: patient && patient.reinternacao ? patient.reinternacao : false,
+    let initialValues = {
+      prontuario: '',
+      data_internacao: '',
+      unidade_primeiro_atendimento: '',
+      unidade_de_saude: '',
+      data_atendimento: '',
+      suporte_respiratorio: false,
+      tipo_suport_respiratorio: '',
+      reinternacao: false,
+    };
+
+    if (patient && patient.prontuario) {
+        initialValues = {
+          prontuario: patient.prontuario,
+          data_internacao: patient.data_internacao,
+          suporte_respiratorio: patient.suporte_respiratorio,
+          reinternacao: patient.reinternacao,
+
+          unidade_primeiro_atendimento: patient.instituicao_primeiro_atendimento.id,
+          unidade_de_saude: patient.instituicao_referencia.id,
+          data_atendimento: patient.data_atendimento_referencia,
+          tipo_suport_respiratorio: patient.tipo_suporte_respiratorios[0].id,
+        }
     }
+
+    return initialValues;
   }
 
   return (
