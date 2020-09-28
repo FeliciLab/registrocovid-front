6cd9a613707768019f4eb350568ad2ffb647e4bd
ericsonmoreira <moreira.ericson@gmail.com>
2020-09-28 17:26:58 -0300

    #90 add chegou_traqueostomizado vindo do backend

--- src/views/GeneralInfo/GeneralInfo.js
+++ src/views/GeneralInfo/GeneralInfo.js
@@ -4,7 +4,7 @@ import useStyles from './styles';
 import { Formik, Form, Field } from 'formik';
 import CustomBreadcrumbs from 'components/CustomBreadcrumbs';
 import { Switch, TextField } from 'formik-material-ui';
-// Material-UI Components
+
 import {
   Typography,
   Button,
@@ -25,7 +25,6 @@ import api from 'services/api';
 import formatDate from 'helpers/formatDate';
 
 const loadInitialValues = patient => {
-  // TODO: colocando aqui o traqueostomizado: false para quando tiver esse campo no backend
   let initialValues = {
     prontuario: '',
     data_internacao: '',
@@ -35,7 +34,7 @@ const loadInitialValues = patient => {
     suporte_respiratorio: false,
     tipo_suport_respiratorio: '',
     reinternacao: false,
-    traqueostomizado: false,
+    chegou_traqueostomizado: false,
   };
 
   if (patient && patient.prontuario) {
@@ -55,7 +54,7 @@ const loadInitialValues = patient => {
         patient.tipo_suporte_respiratorios.length > 0
           ? patient.tipo_suporte_respiratorios[0].id
           : '',
-      traqueostomizado: false,
+      chegou_traqueostomizado: patient.chegou_traqueostomizado,
     };
   }
 
@@ -108,6 +107,7 @@ const GeneralInfo = () => {
       data_atendimento_referencia: values.data_atendimento,
       suporte_respiratorio: values.suporte_respiratorio,
       reinternacao: values.reinternacao,
+      chegou_traqueostomizado: values.chegou_traqueostomizado,
     };
 
     if (values.suporte_respiratorio) {
@@ -139,6 +139,7 @@ const GeneralInfo = () => {
         instituicao_referencia: { id: values.unidade_de_saude },
         data_atendimento_referencia: values.data_atendimento,
         tipo_suporte_respiratorios: [{ id: values.tipo_suport_respiratorio }],
+        chegou_traqueostomizado: values.chegou_traqueostomizado,
       };
 
       addToast({
@@ -394,8 +395,7 @@ const GeneralInfo = () => {
                     </FormGroup>
                   </Grid>
 
-                  {/* TODO: ainda falta colocar esse value nos initial values */}
-                  {/* traqueostomizado */}
+                  {/* chegou_traqueostomizado */}
                   <Grid
                     container
                     item
@@ -406,7 +406,7 @@ const GeneralInfo = () => {
                         <Field
                           color="primary"
                           component={Switch}
-                          name="traqueostomizado"
+                          name="chegou_traqueostomizado"
                           type="checkbox"
                         />
                       }
