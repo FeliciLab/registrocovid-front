e58fd701a70c74cf47aeefaa3fc6b7de402b26a5
MatheusVarela19 <matheus.varela@aluno.uece.br>
2020-09-30 14:56:05 -0300

    #105 minor changes

--- src/layouts/Main/components/Sidebar/Sidebar.js
+++ src/layouts/Main/components/Sidebar/Sidebar.js
@@ -63,8 +63,6 @@ const Sidebar = props => {
         {...rest}
         className={clsx(classes.root, className)}
       >
-        {/* <Profile /> */}
-        {/* <Divider className={classes.divider} /> */}
         <SidebarNav
           className={classes.nav}
           pages={pages}
