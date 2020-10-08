a57837c7e244ff3b2325fa6e6fcd2c432b51eb0b
MatheusVarela19 <matheus.varela@aluno.uece.br>
2020-10-08 15:05:33 -0300

    #107 fixing some conflicts on the GeneralInfo.js

--- src/views/GeneralInfo/GeneralInfo.js
+++ src/views/GeneralInfo/GeneralInfo.js
@@ -163,41 +163,6 @@ const GeneralInfo = () => {
       }
     }
   };
-  
-  const loadInitialValues = () => {
-    let initialValues = {
-      prontuario: '',
-      data_internacao: '',
-      unidade_primeiro_atendimento: '',
-      unidade_de_saude: '',
-      data_atendimento: '',
-      suporte_respiratorio: false,
-      tipo_suport_respiratorio: '',
-      reinternacao: false,
-    };
-
-    if (patient && patient.prontuario) {
-      initialValues = {
-        prontuario: patient.prontuario,
-        data_internacao: patient.data_internacao,
-        suporte_respiratorio: patient.suporte_respiratorio || false,
-        reinternacao: patient.reinternacao || false,
-        unidade_primeiro_atendimento: patient.instituicao_primeiro_atendimento
-          ? patient.instituicao_primeiro_atendimento.id
-          : '',
-        unidade_de_saude: patient.instituicao_referencia
-          ? patient.instituicao_referencia.id
-          : '',
-        data_atendimento: patient.data_atendimento_referencia || '',
-        tipo_suport_respiratorio:
-          patient.tipo_suporte_respiratorios.length > 0
-            ? patient.tipo_suporte_respiratorios[0].id
-            : '',
-      };
-    }
-
-    return initialValues;
-  };
 
   useEffect(() => {
     handleInfos();
