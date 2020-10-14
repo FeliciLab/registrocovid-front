3b30ec2187bee98641f18e72e7db80c9822dc895
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-14 16:02:55 -0300

    #55 add validation data_atendimento >= data_internacao in schema validation

--- src/views/GeneralInfo/schema.js
+++ src/views/GeneralInfo/schema.js
@@ -5,7 +5,16 @@ const schema = Yup.object().shape({
     .integer('Número de prontuário inválido (apenas números inteiros)')
     .min(1, 'Número de prontuário deve ser maior que 0 (zero)')
     .required('Campo obrigatório'),
-  data_internacao: Yup.date().required('Campo obrigatório'),
+  data_internacao: Yup.date()
+    .required('Campo obrigatório'),
+  data_atendimento: Yup.date().when('data_internacao', (data, schema) =>
+    data
+      ? schema.max(
+        data,
+        'Deve ser anterior ou igual a data de internação do paciente',
+      )
+      : schema,
+  ),
 });
 
 export default schema;
