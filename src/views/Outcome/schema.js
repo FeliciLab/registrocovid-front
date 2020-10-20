0c5d8896e8a3cbc451a0d12799b9fc52d89face7
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-20 16:46:50 -0300

    130 add validation  max 1000 chars

--- src/views/Outcome/schema.js
+++ src/views/Outcome/schema.js
@@ -5,6 +5,7 @@ const schema = Yup.object().shape({
     Yup.object().shape({
       tipo_desfecho_id: Yup.number(),
       data: Yup.string().required('Campo obrigatório'),
+      causa_obito: Yup.string().max(1000, 'Máximo de 1000 caracteres'),
     }),
   ),
 });
