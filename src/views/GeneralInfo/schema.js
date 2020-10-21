74e995791c916505dc63712fbc20935ea7986aba
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-21 18:21:04 -0300

    #139 add all validation to card 139

--- src/views/GeneralInfo/schema.js
+++ src/views/GeneralInfo/schema.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import formatDate from 'helpers/formatDate';
 
 const schema = Yup.object().shape({
   prontuario: Yup.number()
@@ -7,7 +8,13 @@ const schema = Yup.object().shape({
     .required('Campo obrigatório'),
   data_internacao: Yup.date()
     .required('Campo obrigatório')
-    .min('01/01/2020', 'Deve ser posterior ou igual à 01/01/2020'),
+    .min('01/01/2020', 'Deve ser posterior ou igual à 01/01/2020')
+    .max(
+      String(new Date()),
+      `Deve ser anterior ou igual a ${new Date().toLocaleString('pt-BR', {
+        dateStyle: 'short',
+      })}`,
+    ),
   data_atendimento: Yup.date()
     .when('data_internacao', (data, schema) =>
       data
@@ -32,6 +39,13 @@ const schema = Yup.object().shape({
           'Deve ser anterior a data do último desfecho (Categoria Desfecho)',
         )
         : schema,
+    )
+    .min('01/01/2020', 'Deve ser posterior ou igual à 01/01/2020')
+    .max(
+      String(new Date()),
+      `Deve ser anterior ou igual a ${new Date().toLocaleString('pt-BR', {
+        dateStyle: 'short',
+      })}`,
     ),
   data_inicio_sintomas: Yup.date(),
   data_ultimo_desfecho: Yup.date(),
