8e061153beebf4fa3dd4d49cbe6aa31fd8e5cb8a
ericsonmoreira <moreira.ericson@gmail.com>
2020-11-24 17:39:02 -0300

    #155 DailyEvolutionList in development

--- src/services/requests/datasRequests.js
+++ src/services/requests/datasRequests.js
@@ -1,24 +1,28 @@
-import api from 'services/api'
+import api from 'services/api';
 
-export const buscarComorbidade = (id) => api.get(`/pacientes/${id}/comorbidades`)
-  .then(response => response.data)
+export const buscarComorbidade = id =>
+  api.get(`/pacientes/${id}/comorbidades`).then(response => response.data);
 
-export const buscarDoencas = () => api.get('/doencas')
-  .then(response => response.data)
+export const buscarDoencas = () =>
+  api.get('/doencas').then(response => response.data);
 
-export const buscarTiposDoencas = () => api.get('/tipos-doencas')
-  .then(response => response.data)
+export const buscarTiposDoencas = () =>
+  api.get('/tipos-doencas').then(response => response.data);
 
-export const buscarCorticosteroides = () => api.get('/corticosteroides')
-  .then(response => response.data)
+export const buscarCorticosteroides = () =>
+  api.get('/corticosteroides').then(response => response.data);
 
-export const buscarOrgaos = () => api.get('/orgaos')
-  .then(response => response.data)
+export const buscarOrgaos = () =>
+  api.get('/orgaos').then(response => response.data);
+
+export const buscarEvolucoesDiarias = id =>
+  api.get(`/pacientes/${id}/evolucoes-diarias`).then(response => response.data);
 
 export default {
   buscarComorbidade,
   buscarTiposDoencas,
   buscarCorticosteroides,
   buscarDoencas,
-  buscarOrgaos
-}
+  buscarOrgaos,
+  buscarEvolucoesDiarias,
+};
--- src/views/DailyEvolutionList/DailyEvolutionList.jsx
+++ src/views/DailyEvolutionList/DailyEvolutionList.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useCallback, useEffect, useState } from 'react';
 import useStyles from './styles';
-import { usePatient } from 'context/PatientContext';
 import DailyEvolutionListBreadcrumbs from './components/DailyEvolutionListBreadcrumbs';
 import DailyEvolutionListTitle from './components/DailyEvolutionListTitle';
+import { buscarEvolucoesDiarias } from 'services/requests/datasRequests';
+import { Typography } from '@material-ui/core';
+import { usePatient } from 'context/PatientContext';
+import { PrevJSON } from 'components';
 
 const DailyEvolutionList = () => {
-  const { patient } = usePatient();
-
   const classes = useStyles();
+
+  const { patient } = usePatient(); // pegando o id o paciente
+
+  const [evolucoesDiarias, setEvolucoesDiarias] = useState([]);
+
+  const handleFecthEvolucoesDiarias = useCallback(async () => {
+    const response = await buscarEvolucoesDiarias(patient.id);
+    console.log(response);
+    setEvolucoesDiarias(response);
+  }, [patient.id]);
+
+  useEffect(() => {
+    handleFecthEvolucoesDiarias();
+  }, [handleFecthEvolucoesDiarias]);
 
   return (
     <div className={classes.root}>
       <DailyEvolutionListBreadcrumbs />
       <DailyEvolutionListTitle />
-      <Typography variant="h1">Evolução diária</Typography>
-      <Typography variant="h1">{patient.id}</Typography>
+      <Typography variant="h4">Lista de evoluções</Typography>
+      <PrevJSON
+        data={evolucoesDiarias}
+        name="evolucoesDiarias"
+      />
     </div>
   );
 };
