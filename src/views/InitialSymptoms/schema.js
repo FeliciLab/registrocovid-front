a14ea8117358642253ee6c851e6907a3374c1680
ericsonmoreira <moreira.ericson@gmail.com>
2020-11-27 14:28:36 -0300

    #169 remove required in data_inicio_sintomas

--- src/views/InitialSymptoms/schema.js
+++ src/views/InitialSymptoms/schema.js
@@ -3,7 +3,6 @@ import * as Yup from 'yup';
 const schema = Yup.object().shape({
   data_internacao: Yup.date(),
   data_inicio_sintomas: Yup.date()
-    .required('Campo obrigatório')
     .min('01/01/2020', 'Deve ser posterior ou igual à 01/01/2020')
     .max(
       Yup.ref('data_internacao'),
