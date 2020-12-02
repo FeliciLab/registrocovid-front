548656892b1bbb47830485b9edd850c46eb3453e
ericsonmoreira <moreira.ericson@gmail.com>
2020-12-02 17:10:22 -0300

    #167 adjusts in layout PersonalHistoryForm

--- src/views/PersonalHistory/styles.js
+++ src/views/PersonalHistory/styles.js
@@ -26,22 +26,6 @@ const useStyles = makeStyles(theme => ({
     width: '258px',
     height: '48px',
   },
-  formContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  form: {
-    backgroundColor: '#fff',
-    width: 864,
-    minHeight: 800,
-    marginTop: 40,
-    paddingLeft: 40,
-    paddingTop: 24,
-  },
-  formGroup: {
-    marginBottom: 24,
-  },
 }));
 
 export default useStyles;
